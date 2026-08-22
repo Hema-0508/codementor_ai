74f652702d645b47ff165b19b12970a820f4b327
hema <hemabangaru3@gmail.com>
2026-08-22 20:39:25 +0530

    add user registeration functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 def register(request):
-    return render(request,'register.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password == confirm_password:
+            User.objects.create_user(username=username,
+                                     email=email,
+                                     password=password)
+            return render(request, 'register.html')
+    return render(request, 'register.html')
