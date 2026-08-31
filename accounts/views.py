3d3a42bbbfed785f6761ee85461c08aa41699772
hema <hemabangaru3@gmail.com>
2026-08-31 21:38:07 +0530

    Add registration validation and messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -11,9 +12,12 @@ def register(request):
         password = request.POST.get('password')
         confirm_password = request.POST.get('confirm_password')
 
-        if password == confirm_password:
-            User.objects.create_user(username=username,
-                                     email=email,
-                                     password=password)
+        if password != confirm_password:
+            messages.error(request, 'passwords do not match.')
             return render(request, 'register.html')
+        User.objects.create_user(username=username,
+                                 email=email,
+                                 password=password)
+        messages.success(request, 'Registration successful.')
+        return render(request, 'register.html')
     return render(request, 'register.html')
